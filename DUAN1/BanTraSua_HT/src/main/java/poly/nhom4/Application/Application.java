bea89396b69c9a00c69b6a3945b309fe3feb67b2
Quanndaph26528 <quanndaph26528@fpt.edu.vn>
2022-11-28 01:10:21 +0700

    Thêm form đăng nhập, đăng ký, quên mk, đổi mk

---
+++ DUAN1/BanTraSua_HT/src/main/java/poly/nhom4/Application/Application.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poly.nhom4.Application;
+
+//import Views.Quanndaph26528;
+import poly.nhom4.view.DangNhapForm;
+
+/**
+ *
+ * @author Admin
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+        DangNhapForm dn = new DangNhapForm();
+
+        dn.setLocationRelativeTo(null);
+
+        dn.setVisible(true);
+
+//            this.dispose();
+    }
+}
